4c8152892679bd4cc02bbac3c3df7caeead589d2
longpod97 <175112945+longpod97@users.noreply.github.com>
2024-07-24 14:19:28 +0800

    Progress. Doesn't quite work yet though. Throwing wrong win/lose statements

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # r-p-s
 Rock Paper Scissors game
+
+Pseudo code:
+
+RPS Array - Rock[0], Paper[1], Scissors[2]
+
+Func - Get human input
+    prompt - rock paper scissors?
+    floor to lower case
+    Is it a valid input? {
+            If no - prompt again with error message
+            else continue to game round }
+
+Func - get computer input
+    random number 0 - 2
+    apply number to rps array
+
+Function scores
+    if PC === CC then its a draw
+
+    if PC = rock && CC = paper then you lose
+
+    if pc = rock && CC = scissors then you win
+
+    if PC = Paper && CC = rock then you win
+
+    if pc = paper && CC = scissors then you lose
+
+    if PC = scissors && CC = paper then you win
+
+    if pc = scissors && CC = rock then you lose
---
+++ rps.js
@@ -0,0 +1,75 @@
+const rps = ["rock", "paper", "scissors"];
+
+let humanScore = 0;
+let compScore = 0;
+
+const comp = getCompChoice();
+const human = getHumanChoice();
+
+function getHumanChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt("Rock, Paper or Scissors?");
+        if (choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if (rps.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
+
+function getCompChoice() {
+    let i = Math.floor(Math.random()*2);
+    return rps[i];
+}
+
+/* function getHumanChoice() {
+    const playerChoice = window.prompt("Rock, Paper or Scissors?");
+    return playerChoice;
+} */
+
+function playRound(player, computer) {
+    console.log("Your choice: " + human);
+    console.log("Computer choice: " + comp);
+    if (player == computer) {
+        console.log("It's a draw!")
+    } else if (player === "rock" && computer === "paper") {
+        console.log("You lose, paper beats rock");
+        compScore ++;
+        console.log("Your score: " + humanScore);
+        console.log("Computer Score: " + compScore);
+    } else if (player === "rock" && computer === "scissors") {
+        console.log("You win, rock beats scissors");
+        humanScore ++;
+        console.log("Your score: " + humanScore);
+        console.log("Computer Score: " + compScore);
+    } else if (player === "paper" && computer === "rock") {
+        console.log("You win, paper beats rock");
+        humanScore ++;
+        console.log("Your score: " + humanScore);
+        console.log("Computer Score: " + compScore);
+    } else if (player === "paper" && computer === "scissors") {
+        console.log("You lose, scissors beats paper");
+        compScore ++;
+        console.log("Your score: " + humanScore);
+        console.log("Computer Score: " + compScore);
+    } else if (player === "scissors" && computer === "rock") {
+        console.log("You lose, rock beats scissors");
+        compScore ++;
+        console.log("Your score: " + humanScore);
+        console.log("Computer Score: " + compScore);
+    } else if (player === "scissors" && computer === "paper") {
+        console.log("You win, scissors beats paper");
+        humanScore ++;
+        console.log("Your score: " + humanScore);
+        console.log("Computer Score: " + compScore);
+    }
+}
+
+
+
+playRound(getHumanChoice(), getCompChoice());
